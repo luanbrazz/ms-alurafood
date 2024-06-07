91a3807b89d351be5a83f619784264e3f349612a
Luan Braz <luan_silvacpv@hotmail.com>
2024-06-07 19:17:20 -0300

    finished payment crud

---
+++ src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
@@ -0,0 +1,53 @@
+package br.com.alurafood.pagamentos.controller;
+
+import br.com.alurafood.pagamentos.dto.PagamentoDto;
+import br.com.alurafood.pagamentos.service.PagamentoService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/pagamentos")
+public class PagamentoController {
+
+    @Autowired
+    private PagamentoService pagamentoService;
+
+    @GetMapping
+    public Page<PagamentoDto> obterTodos(@PageableDefault(size = 10) Pageable pageable) {
+        return pagamentoService.obterTodos(pageable);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PagamentoDto> obterPorId(@PathVariable @NotNull Long id) {
+        PagamentoDto pagamentoDto = pagamentoService.obterPorId(id);
+        return ResponseEntity.ok(pagamentoDto);
+    }
+
+    @PostMapping
+    public ResponseEntity<PagamentoDto> criarPagamento(@RequestBody @Valid PagamentoDto pagamentoDto, UriComponentsBuilder uriBuilder) {
+        PagamentoDto dto = pagamentoService.criarPagamento(pagamentoDto);
+        URI endereco = uriBuilder.path("/pagamentos/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(endereco).body(dto);
+    }
+
+    @PutMapping
+    public ResponseEntity<PagamentoDto> atualizarPagamento(@PathVariable @NotNull Long id, @RequestBody @Valid PagamentoDto pagamentoDto) {
+      PagamentoDto pgAtualizado = pagamentoService.atualizarPagamento(id, pagamentoDto);
+      return ResponseEntity.ok(pgAtualizado);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<PagamentoDto> removerPagamento(@PathVariable @NotNull Long id) {
+        pagamentoService.removerPagamento(id);
+        return ResponseEntity.noContent().build();
+    }
+}
