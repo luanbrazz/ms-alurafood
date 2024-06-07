8f8c692cd8c01cf9848b7de542dd0571c466a96d
Luan Braz <luan_silvacpv@hotmail.com>
2024-06-07 15:29:53 -0300

    created service for payment

--- src/main/java/br/com/alurafood/pagamentos/service/PagamentoService.java
+++ src/main/java/br/com/alurafood/pagamentos/service/PagamentoService.java
@@ -2,6 +2,7 @@ package br.com.alurafood.pagamentos.service;
 
 import br.com.alurafood.pagamentos.dto.PagamentoDto;
 import br.com.alurafood.pagamentos.model.Pagamento;
+import br.com.alurafood.pagamentos.model.enumm.Status;
 import br.com.alurafood.pagamentos.repository.PagamentoRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.modelmapper.ModelMapper;
@@ -27,5 +28,24 @@ public class PagamentoService {
         Pagamento pagamento = pagamentoRepository.findById(id).orElseThrow(() -> new EntityNotFoundException());
 
         return modelMapper.map(pagamento, PagamentoDto.class);
+    }
+
+    public PagamentoDto criarPagamento(PagamentoDto pagamentoDto) {
+        Pagamento pagamento = modelMapper.map(pagamentoDto, Pagamento.class);
+        pagamento.setStatus(Status.CRIADO);
+        pagamentoRepository.save(pagamento);
+
+        return modelMapper.map(pagamento, PagamentoDto.class);
+    }
+
+    public PagamentoDto atualizarPagamento(Long id, PagamentoDto pagamentoDto) {
+        Pagamento pagamento = modelMapper.map(pagamentoDto, Pagamento.class);
+        pagamento.setId(id);
+        pagamento = pagamentoRepository.save(pagamento);
+        return modelMapper.map(pagamento, PagamentoDto.class);
+    }
+
+    public void removerPagamento(Long id) {
+        pagamentoRepository.deleteById(id);
     }
 }
